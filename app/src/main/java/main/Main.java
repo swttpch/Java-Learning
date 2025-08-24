ffdbe9d5fe3db66ef3cb2cb453c12d78bde499cc
swttpch <igsouza.contato@gmail.com>
2025-08-24 15:47:11 -0300

    chore: add `whileLoop`

--- app/src/main/java/main/Main.java
+++ app/src/main/java/main/Main.java
@@ -3,11 +3,10 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        WrapperClasses test = new WrapperClasses();
-        System.out.println(test.a); // true
-        System.out.println(test.b); //  @
-        System.out.println(test.c); // 123
-        System.out.println(test.d); // 3.14
-        System.out.println(test.e); //  Igor
+       WhileLoop whileLoop = new WhileLoop();
+
+       whileLoop.doApproach();
+
+       // whileLoop.whileApproach();
     }
 }
